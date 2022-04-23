495d7ca46e3593a4c1215bddb2a37549538759e4
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2022-04-23 13:10:56 +0900

    commit

---
+++ Assets/class_method.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class class_method : MonoBehaviour
+{
+  // Start is called before the first frame update
+
+  /* ƒNƒ‰ƒX
+     int, string, ... */
+  void Start()
+  {
+    PlayerModel player = new PlayerModel();
+    player.SayName();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+        
+  }
+}
