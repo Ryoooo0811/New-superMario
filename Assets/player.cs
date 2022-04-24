16478e75aa336fb69d115f53adb2bb1d3d0b77f7
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2022-04-24 21:23:28 +0900

    input item

---
+++ Assets/player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+  Animator animator;
+  // Start is called before the first frame update
+  void Start()
+  {
+    animator = GetComponent<Animator>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    float x = Input.GetAxis("Horizontal");
+    if (x > 0)
+    {
+      animator.SetFloat("speed", x);
+    }
+    else if (x == 0)
+    {
+      animator.SetFloat("speed", x);
+    }
+  }
+}
